bc6b0e30b36b22d760f2efdc3c6ee1c582bc9597
bLaKtivist <hello@krysfreeman.com>
2015-03-30 21:24:31 -0700

    Initial commit

---
+++ client/lib/app.js
@@ -0,0 +1,15 @@
+angular.module('hella',[
+  'angular-meteor',
+  'ui.router',
+  'angularUtils.directives.dirPagination',
+  'uiGmapgoogle-maps'
+]);
+
+function onReady() {
+  angular.bootstrap(document, ['hella']);
+}
+
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
